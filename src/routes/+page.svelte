28f50524b0b0b7e0ee2555b426bcf254f1ff7f9e
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-16 17:01:26 +0530

    feat: update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	import { Github } from 'lucide-svelte';
+	import Logo from '$lib/components/ui/logo/logo.svelte';
 </script>
 
 <div class="flex h-screen items-center justify-center">
 	<div>
 		<div class="bg-secondary mb-4 flex border p-4">
 			<div class="pr-8">
-				<h1 class="mb-2 text-xl font-semibold">Slate</h1>
+				<h1 class="mb-4 flex items-center text-xl font-semibold">
+					<Logo />
+					Slate
+				</h1>
 				<p class="text-muted-foreground mb-4">
 					Open source chat UI for local,
 					<br />
 					self-hosted and cloud llms.
 				</p>
-				<a target="_blank" href="https://github.com/aritra1999/slate"><Github class="size-5" /></a>
+				<a class="underline" target="_blank" href="https://github.com/aritra1999/slate">
+					Want to contribute?
+				</a>
 			</div>
 			<img
 				src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGs5bTk2YWJ2dzliYXJyeGF6cjkycjk3NXEwZ2RpMjd1NGpwMzVvMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/W6A8UMnaJj8YbM9CQw/giphy.gif"
