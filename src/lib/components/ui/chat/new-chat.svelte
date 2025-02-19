d18cec92c56ad954af760a3be4737e7116ede942
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-19 09:11:33 +0530

    feat: add new chat component

---
+++ src/lib/components/ui/chat/new-chat.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { SquarePen } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<Button variant="ghost" size="icon" class="rounded-full">
+	<SquarePen />
+</Button>
