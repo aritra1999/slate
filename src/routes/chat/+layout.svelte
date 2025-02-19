5061f4aba11384539e85d0abd0b91bb61c6db4c0
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-19 09:12:53 +0530

    feat: add chat sidebar layout

---
+++ src/routes/chat/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import AppSidebar from '$lib/components/ui/app-sidebar/app-sidebar.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header class="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+			<Sidebar.Trigger class="-ml-1" />
+		</header>
+		{@render children?.()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
 	import { Paperclip, Send } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
+	import ModelDetails from '$lib/components/ui/model-details/model-details.svelte';
 </script>
 
-<div class="flex h-screen w-full">
+<div class="flex h-full w-full flex-col">
 	<div class="container mx-auto flex h-full flex-col">
-		<div class="flex-none py-4">header</div>
 		<div class="flex-1 overflow-y-auto">chat section</div>
 		<div class="flex-none py-4">
-			<form class="rounded-3xl border-2 bg-white p-2">
+			<form class="rounded-3xl border-2 bg-sidebar p-2">
 				<textarea
 					class="w-full resize-none overflow-y-auto bg-transparent px-3 py-2.5 placeholder:text-muted-foreground focus:outline-0"
 					placeholder="What's on your mind?"
 					rows="3"
 				></textarea>
 				<div class="flex items-start justify-between">
-					<Button class="rounded-2xl" disabled>
-						<span>Attach</span>
-						<Paperclip class="ml-2" />
-					</Button>
+					<div class="flex space-x-2">
+						<ModelDetails />
+						<Button class="rounded-2xl" size="icon" disabled>
+							<Paperclip />
+						</Button>
+					</div>
 					<Button class="rounded-2xl" size="icon">
 						<Send />
 					</Button>
