e193e71d6ce03b5035249a0c272cd62bc7a23897
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-19 09:11:12 +0530

    feat: add sidebar

---
+++ src/lib/components/ui/app-sidebar/theme.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	import { mode, toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Button onclick={toggleMode} variant="secondary" class="px-4" size="icon">
+	{#if $mode === 'light'}
+		<Sun class="h-[1.2rem] w-[1.2rem]" />
+	{:else}
+		<Moon class="h-[1.2rem] w-[1.2rem]" />
+	{/if}
+</Button>
