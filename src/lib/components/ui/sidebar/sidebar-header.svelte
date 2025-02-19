bef2a749a89fee755654cbc0580ee1f58f50fbc3
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-19 09:12:24 +0530

    shadcn: update sidebar

--- src/lib/components/ui/sidebar/sidebar-header.svelte
+++ src/lib/components/ui/sidebar/sidebar-header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import type { WithElementRef } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from 'svelte/elements';
+	import type { WithElementRef } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
 
 	let {
 		ref = $bindable(null),
@@ -14,7 +14,7 @@
 <div
 	bind:this={ref}
 	data-sidebar="header"
-	class={cn("flex flex-col gap-2 p-2", className)}
+	class={cn('flex h-16 items-center gap-2 px-4', className)}
 	{...restProps}
 >
 	{@render children?.()}
--- src/lib/components/ui/sidebar/sidebar-trigger.svelte
+++ src/lib/components/ui/sidebar/sidebar-trigger.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils.js";
-	import PanelLeft from "lucide-svelte/icons/panel-left";
-	import type { ComponentProps } from "svelte";
-	import { useSidebar } from "./context.svelte.js";
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+	import AlignLeft from 'lucide-svelte/icons/align-left';
+	import type { ComponentProps } from 'svelte';
+	import { useSidebar } from './context.svelte.js';
 
 	let {
 		ref = $bindable(null),
@@ -26,9 +26,9 @@
 	data-sidebar="trigger"
 	variant="ghost"
 	size="icon"
-	class={cn("h-7 w-7", className)}
+	class={cn(' h-10 w-10 rounded-full', className)}
 	{...restProps}
 >
-	<PanelLeft />
+	<AlignLeft class="h-8 w-8" />
 	<span class="sr-only">Toggle Sidebar</span>
 </Button>
