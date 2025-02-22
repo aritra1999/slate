34e34d85134d0a6aca38793413552dd43916c67f
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-22 21:20:29 +0530

    feat: add models store

---
+++ src/lib/store/model.store.ts
@@ -0,0 +1,23 @@
+import type { Model } from '$lib/types';
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+const storedModels = browser ? localStorage.getItem('models') : null;
+const initialModels: Model[] = storedModels ? JSON.parse(storedModels) : [];
+
+const store = writable<Model[]>(initialModels);
+
+export const modelStore = {
+	subscribe: store.subscribe,
+	set: (value: Model[]) => {
+		localStorage.setItem('models', JSON.stringify(value));
+		store.set(value);
+	},
+	update: (updater: (value: Model[]) => Model[]) => {
+		store.update((value) => {
+			const updatedValue = updater(value);
+			if (browser) localStorage.setItem('models', JSON.stringify(updatedValue));
+			return updatedValue;
+		});
+	}
+};
