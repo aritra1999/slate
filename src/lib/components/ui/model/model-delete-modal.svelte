9b09cdcba533c41ee1cb9adc67e46a724dae0a17
Aritra Mondal <aritra041999.dev@gmail.com>
2025-02-28 20:52:05 +0100

    feat: add model delete icon

--- src/lib/components/ui/model/model-delete-modal.svelte
+++ src/lib/components/ui/model/model-delete-modal.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
+	import { Trash } from 'lucide-svelte';
 	import type { Model } from '$lib/types';
 	import { modelStore } from '$lib/store/model.store';
 	import { buttonVariants } from '../button/button.svelte';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
 
 	let { model }: { model: Model } = $props();
+	let open = $state(false);
 
 	async function deleteModel() {
 		modelStore.update((models: Model[]) => models.filter((m) => m.id !== model.id));
+		open = false;
 	}
 </script>
 
-<AlertDialog.Root>
-	<AlertDialog.Trigger>Delete</AlertDialog.Trigger>
+<AlertDialog.Root bind:open>
+	<AlertDialog.Trigger>
+		<Trash class="size-4 hover:text-red-400" />
+	</AlertDialog.Trigger>
 	<AlertDialog.Content>
 		<AlertDialog.Header>
 			<AlertDialog.Title>
@@ -21,6 +26,7 @@
 			<AlertDialog.Description>This action cannot be undone.</AlertDialog.Description>
 		</AlertDialog.Header>
 		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
 			<AlertDialog.Action class={buttonVariants({ variant: 'destructive' })} onclick={deleteModel}>
 				Delete
 			</AlertDialog.Action>
