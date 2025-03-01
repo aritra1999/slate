40de5966d7fe3845b467e434164c131f0355c8b9
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-02 00:00:54 +0100

    feat: update chat form

--- src/lib/components/ui/chat/new-chat.svelte
+++ src/lib/components/ui/chat/new-chat.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import LocalChatForm from '$lib/components/ui/chat/chat-form.svelte';
+	import { metaStore } from '$lib/store/meta.store';
 
 	let { children } = $props();
 </script>
 
-<Dialog.Root>
+<Dialog.Root bind:open={$metaStore.showChatForm}>
 	{@render children()}
 	<Dialog.Content>
 		<Dialog.Header>
