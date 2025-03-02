79faad4f3fc5c6b4c9cc11ad76a2f102289a3530
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-02 19:46:55 +0100

    feat: add function for communicating to ollama

--- src/lib/services/ollama.ts
+++ src/lib/services/ollama.ts
@@ -1,6 +1,6 @@
 import { OLLAMA_API_URL } from '$lib/config';
 import { err, ok, type Result } from '$lib/error';
-import type { OllamaModel } from '$lib/types';
+import type { Message, OllamaModel } from '$lib/types';
 import ky from 'ky';
 
 export async function getOllamaModels(): Promise<Result<OllamaModel[]>> {
@@ -15,6 +15,31 @@ export async function getOllamaModels(): Promise<Result<OllamaModel[]>> {
 			})
 			.json<{ models: OllamaModel[] }>();
 		return ok(modelsResponse.models);
+	} catch (error) {
+		return err(error);
+	}
+}
+
+export async function communicateToOllamaModel(
+	modelName: string,
+	messages: Message[]
+): Promise<Result<Message>> {
+	try {
+		const chatResponse = await ky
+			.post(`${OLLAMA_API_URL}/api/chat`, {
+				retry: 0,
+				headers: {
+					'content-type': 'application/json'
+				},
+				json: {
+					model: modelName,
+					messages: messages
+				},
+				referrerPolicy: 'strict-origin-when-cross-origin'
+			})
+			.json<{ message: Message }>();
+
+		return ok(chatResponse.message);
 	} catch (error) {
 		return err(error);
 	}
