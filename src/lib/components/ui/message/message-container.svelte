9bc2f886be5922bb83c444d7bb40f9032ff3863c
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-02 19:45:58 +0100

    feat: add message container

---
+++ src/lib/components/ui/message/message-container.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Chat, Message } from '$lib/types';
+	import MessageBubble from '$lib/components/ui/message/message-bubble.svelte';
+	import { chatStore, selectedChatStore } from '$lib/store/chat.store';
+
+	let selectedChat = $derived<Chat | undefined>(
+		$selectedChatStore ? $chatStore.get($selectedChatStore) : undefined
+	);
+
+	let messages = $derived<Message[]>(selectedChat ? selectedChat.messages : []);
+</script>
+
+{#each messages as message}
+	<MessageBubble role={message.role} class="">{message.content}</MessageBubble>
+{/each}
