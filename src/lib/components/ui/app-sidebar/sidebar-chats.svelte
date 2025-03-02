e773d0491e75c4b8e2dae7cce7e8e010633ecea7
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-02 19:45:01 +0100

    feat: update sidebar chat section

--- src/lib/components/ui/app-sidebar/sidebar-chats.svelte
+++ src/lib/components/ui/app-sidebar/sidebar-chats.svelte
@@ -7,6 +7,8 @@
 	import { buttonVariants } from '$lib/components/ui/button/index.js';
 	import type { Chat } from '$lib/types';
 
+	let chatsArray = $derived<Chat[]>(Array.from($chatStore.values()));
+
 	function selectChat(chat: Chat) {
 		selectedChatStore.set(chat.id);
 	}
@@ -15,12 +17,18 @@
 <Sidebar.MenuItem class="mb-4">
 	<Sidebar.MenuButton class="text-base font-medium">Chats</Sidebar.MenuButton>
 	<Sidebar.MenuSub>
-		{#if $chatStore.length > 0}
-			{#each $chatStore as chat}
+		{#if chatsArray.length > 0}
+			{#each chatsArray as chat}
 				<Sidebar.MenuSubItem>
 					<Sidebar.MenuSubButton>
 						{#snippet child({ props })}
-							<button {...props} onclick={() => selectChat(chat)}>
+							<button
+								{...props}
+								onclick={() => selectChat(chat)}
+								class={cn('w-full rounded-md px-4 py-3 no-underline hover:bg-sidebar-accent', {
+									'bg-sidebar-accent': $selectedChatStore === chat.id
+								})}
+							>
 								<div class="text-left">
 									<div>{chat.title}</div>
 									<div class="text-xs text-muted-foreground">
