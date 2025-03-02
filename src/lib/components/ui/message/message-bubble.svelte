3a3c92121eb7662ac471808b443e5313603c3f1e
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-02 23:06:10 +0100

    feat: make ai response background transparent

--- src/lib/components/ui/message/message-bubble.svelte
+++ src/lib/components/ui/message/message-bubble.svelte
@@ -6,8 +6,8 @@
 	let isThinking = $derived(role === 'assistant' && (!content || content.trim() === ''));
 </script>
 
-<div class={cn(role === 'user' ? 'justify-end' : 'justify-start', 'flex w-full')}>
-	<div class={cn(role === 'user' ? 'text-right' : 'text-left', 'mb-4 flex max-w-4xl space-x-2')}>
+<div class={cn(role === 'user' ? 'justify-end' : 'justify-start', 'flex w-full', 'my-4')}>
+	<div class={cn(role === 'user' ? 'text-right' : 'text-left', 'mb-4 flex max-w-3xl space-x-2')}>
 		{#if role === 'system' || role === 'assistant'}
 			<div class="flex size-10 items-center justify-center rounded-lg bg-violet-400 p-2.5">
 				<Sparkles class="size-5 text-slate-950" />
@@ -15,13 +15,13 @@
 		{/if}
 		<div
 			class={cn(
-				'w-full rounded-2xl p-6',
-				role === 'user' ? 'rounded-tr-sm bg-accent' : 'rounded-tl-sm bg-secondary',
+				'w-full ',
+				role === 'user' ? 'rounded-2xl rounded-tr-sm bg-accent p-6' : 'px-4',
 				className
 			)}
 		>
 			{#if isThinking}
-				<div class="flex items-center text-muted-foreground">
+				<div class="flex items-center py-4 text-muted-foreground">
 					<Loader2 class="mr-2 size-4 animate-spin" />
 					<span>Thinking...</span>
 				</div>
