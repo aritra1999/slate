50aa1b2e8ffda9b0ed2e6db39f935b005f35e87e
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-26 11:42:49 +0100

    feat: add feedback route

---
+++ src/routes/feedback/+page.svelte
@@ -0,0 +1,10 @@
+<div class="h-screen w-screen">
+	<iframe
+		src="https://heavy-riverbed-c1d.notion.site/ebd/1c2a731b219b80ef8b51ce897024beb5"
+		width="100%"
+		height="100%"
+		title="Feedback Form"
+		frameborder="0"
+		allowfullscreen
+	></iframe>
+</div>
