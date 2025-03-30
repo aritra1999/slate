57b155ef2afe55ecee65f808c61a46f0190e9bab
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-30 18:25:28 +0200

    feat: update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,127 @@
 <script lang="ts">
 	import Logo from '$lib/components/ui/logo/logo.svelte';
+	import Theme from '$lib/components/ui/app-sidebar/theme.svelte';
+	import BentoGrid from '$lib/components/ui/special/bento-grid.svelte';
+	import BentoItem from '$lib/components/ui/special/bento-item.svelte';
+	import SparklesText from '$lib/components/ui/special/sparkels-text.svelte';
+	import Bot from 'lucide-svelte/icons/bot';
+	import BotMessageSquare from 'lucide-svelte/icons/bot-message-square';
+
+	import Lock from 'lucide-svelte/icons/lock';
+
+	import MessageSquare from 'lucide-svelte/icons/message-square';
+
+	import LogoIcon from '$lib/components/ui/logo/logo-icon.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import DynamicImage from '$lib/components/ui/special/dynamic-image.svelte';
+	import Github from 'lucide-svelte/icons/github';
+	import Banana from 'lucide-svelte/icons/banana';
+
+	const thumbnail = {
+		light: 'https://ucarecdn.com/25a7ef27-2fc3-4bbd-82b5-5cc975117891/-/preview/1000x628/',
+		dark: 'https://ucarecdn.com/cddee9ea-0f1b-4a1f-a4f2-163522a0d7ab/-/preview/1000x628/'
+	};
+
+	const features = [
+		{
+			icon: Github,
+			description:
+				'Fully open source and community-driven. Contribute, modify, and extend Slate to fit your specific needs.',
+			feature: 'Open Source'
+		},
+		{
+			icon: Bot,
+			description:
+				'Configure and use multiple models simultaneously, with customizable parameters like temperature.',
+			feature: 'Multiple Model Support'
+		},
+		{
+			icon: Banana,
+			description: 'No need to run docker containers for the UI.',
+			feature: 'Our UI'
+		},
+		{
+			icon: Lock,
+			description:
+				'Keep your conversations and data private by running everything locally, with no data leaving your device.',
+			feature: 'Privacy-Focused'
+		}
+	];
 </script>
 
-<div class="flex h-screen items-center justify-center px-4">
-	<div>
-		<div class="mb-4 flex flex-col-reverse border bg-secondary p-4 sm:flex-row">
-			<div class="pr-8">
-				<div class="mb-6">
-					<Logo />
-				</div>
-				<p class="mb-5 text-muted-foreground">
-					Open source chat UI for local,
-					<br />
-					self-hosted and cloud llms.
-				</p>
-				<div class="mb-3">
-					<a href="/chat"> Chat </a>
-				</div>
-				<div class="mb-3">
-					<a target="_blank" href="https://github.com/aritra1999/slate">Want to contribute?</a>
-				</div>
-				<div class="mb-3">
-					<a target="_blank" href="/feedback">Got feedback?</a>
-				</div>
+<main class="container mx-auto py-10 sm:py-24">
+	<BentoGrid className="mb-4">
+		<BentoItem className="col-span-1 sm:col-span-2 lg:col-span-1 sm:row-span-1 p-6">
+			<div class="flex items-center gap-3">
+				<LogoIcon />
+				<SparklesText text="slate" />
 			</div>
-			<img
-				src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGs5bTk2YWJ2dzliYXJyeGF6cjkycjk3NXEwZ2RpMjd1NGpwMzVvMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/W6A8UMnaJj8YbM9CQw/giphy.gif"
-				class="mb-6 h-56 w-full object-cover object-center sm:mb-0 sm:w-40"
-				alt="Animated coming soon illustration"
-			/>
-		</div>
-	</div>
-</div>
+			<p class="mb-10 text-base text-muted-foreground">
+				Our open source UI, your models.
+				<br />
+				Local, self-hosted and cloud llms.
+			</p>
+			<div>
+				<Button variant="accent" size="lg" href="/chat" class="mr-2 rounded-lg px-6 no-underline">
+					Chat
+					<MessageSquare />
+				</Button>
+				<Button
+					size="lg"
+					target="_blank"
+					href="https://github.com/aritra1999/slate"
+					class="rounded-lg px-6 no-underline"
+				>
+					Contribute
+					<Github />
+				</Button>
+			</div>
+		</BentoItem>
+		<BentoItem className="col-span-1 row-span-1 sm:col-span-2 sm:row-span-2 p-2">
+			<DynamicImage images={thumbnail} />
+		</BentoItem>
+		<BentoItem className="col-span-1 row-span-2 p-2">
+			<div class="flex h-full flex-col justify-evenly gap-2">
+				{#each features as { icon: Icon, feature, description }}
+					<div class=" h-full rounded-lg bg-background p-4">
+						<h2 class="mb-3 flex items-center gap-4 text-sm font-semibold">
+							<Icon class="size-5" />
+							{feature}
+						</h2>
+						<p class="text-sm text-muted-foreground">{description}</p>
+					</div>
+				{/each}
+			</div>
+		</BentoItem>
+		<BentoItem className="col-span-1 p-2">
+			<div class="relative h-full w-full">
+				<div class="absolute bottom-4 right-4 rounded-md bg-lime-300 px-4 py-2 text-neutral-900">
+					Work In Progress 🚧
+				</div>
+				<img
+					src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW83c2F3dzFuMDF5bGwwNGF3amViMngyMDVsd2pjY2d4cHh3MWtraCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ZTans30ONaaIM/giphy.gif"
+					class=" h-full w-full rounded-lg object-cover"
+					alt=""
+				/>
+			</div>
+		</BentoItem>
+		<BentoItem className="col-span-1 p-6">
+			<Logo />
+			<div class="flex w-fit flex-col gap-2 px-1 py-2">
+				<a class="no-underline hover:underline" href="/chat"> Chat </a>
+				<a
+					class="no-underline hover:underline"
+					target="_blank"
+					href="https://github.com/aritra1999/slate">Want to contribute?</a
+				>
+				<a class="no-underline hover:underline" target="_blank" href="/feedback">Got feedback?</a>
+			</div>
+			<div class="flex w-full items-center justify-between rounded-xl">
+				<div class="flex items-center gap-8 text-sm text-muted-foreground">
+					<p>&copy; 2025 Slate. All rights reserved.</p>
+				</div>
+				<Theme />
+			</div>
+		</BentoItem>
+	</BentoGrid>
+</main>
