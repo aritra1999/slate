eb462e8207bbbb7e866b17059ecab83382109879
Aritra Mondal <aritra041999.dev@gmail.com>
2025-03-30 19:04:39 +0200

    feat: init posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import { PUBLIC_POSTHOG_TOKEN } from '$env/static/public';
+
 	let { children } = $props();
+
+	onMount(() => {
+		if (browser) {
+			posthog.init(PUBLIC_POSTHOG_TOKEN, {
+				api_host: 'https://eu.i.posthog.com',
+				person_profiles: 'always'
+			});
+		}
+		return;
+	});
 </script>
 
 <svelte:head>
